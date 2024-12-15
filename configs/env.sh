0b80e10900d6c69cede6d7531e1e7595888d32c0
Raul del Aguila <38803643+raulaguila@users.noreply.github.com>
2024-12-15 01:33:30 -0400

    updated

--- configs/env.sh
+++ configs/env.sh
@@ -36,8 +36,4 @@ POSTGRES_PORT='5432'                            # Postgres Container PORT
 POSTGRES_USER='root'                            # Postgres USER
 POSTGRES_PASS='root'                            # Postgres PASS
 POSTGRES_BASE='go_api'                          # Postgres BASE
-
-ADM_NAME='Administrator'                        # User Default Name
-ADM_MAIL='admin@admin.com'                      # User Default Email
-ADM_PASS='12345678'                             # User Default Pass
 " >.env
